d0d235864612f9ebc253a5f2f1a941296556d0c6
João Paulo Alencar <joaosvalencar@Joaos-MacBook-Pro-2.local>
2026-07-09 18:14:06 +1000

    updated list_br, reviewed testudines and br species from RDB

---
+++ R/D_exploratory.R
@@ -0,0 +1,163 @@
+library(letsRept)
+reptCompare(list$species, filter = "review") # all nomenclature up to date
+
+# RDB Brazilian species check ---------------------------------------------
+link <- reptAdvancedSearch(location = "Brazil")
+br_species <- reptSpecies(link, taxonomicInfo = FALSE, cores = 9)
+
+missing <- br_species[!br_species$species %in% list_br$species,]
+
+# checking which missing species actually occur in BR ---------------------
+missing$species
+
+# selecting species to drop from RDB Brazilian list -----------------------
+# most are invasive, the other have poor evidence of occurrence in Brazilian territory
+
+br_drop <- c("Anolis porcatus",
+             "Anolis sagrei",
+             "Bachia alleni",
+             "Gekko gecko",
+             "Gonatodes concinnatus",
+             "Hemidactylus mabouia",
+             "Leiosaurus paronae",
+             "Lepidodactylus lugubris",
+             "Ophiodes vertebralis",
+             "Pantherophis guttatus",
+             "Pelodiscus sinensis",
+             "Phalotris spegazzinii",
+             "Plica kathleenae",
+             "Trachemys scripta",
+             "Trachylepis maculata",
+             "Tropidurus guarani",
+             "Tropidurus spinulosus",
+             "Urotheca multilineata")
+
+missing <- missing[!missing$species %in% br_drop,] #drop from the missing list
+br_species <- br_species[!br_species$species %in% br_drop,] # drop from RDB Brazilian list N = 878 species
+
+# check which missing species occur in the Cerrado ------------------------
+
+list_br$cerrado_endemic <- ifelse(list_br$species %in% end$species, "yes", "no") #adding binary endemism information from Vieira-Alencar et al. 2023
+
+list_br$species[list_br$cerrado_endemic == "yes" & list_br$cerrado_sp == "no"]
+list_br$cerrado_sp[list_br$cerrado_endemic == "yes" & list_br$cerrado_sp == "no"] <- "yes"
+
+new_species <- data.frame()
+
+toAdd <- data.frame(order = "Squamata",
+                    family = "Colubridae",
+                    species = "Chironius xaraes",
+                    biomes = "Cerrado, Pantanal",
+                    cerrado_sp = "yes",
+                    cerrado_endemic = "no")
+
+new_species <- rbind(new_species, toAdd)
+
+toAdd <- data.frame(order = "Squamata",
+                    family = "Scincidae",
+                    species = "Copeoglossum oreades",
+                    biomes = "Cerrado",
+                    cerrado_sp = "yes",
+                    cerrado_endemic = "yes")
+
+new_species <- rbind(new_species, toAdd)
+
+toAdd <- data.frame(order = "Squamata",
+                    family = "Colubridae",
+                    species = "Leptophis mystacinus",
+                    biomes = "Cerrado",
+                    cerrado_sp = "yes",
+                    cerrado_endemic = "yes")
+
+new_species <- rbind(new_species, toAdd)
+
+toAdd <- data.frame(order = "Squamata",
+                    family = "Elapidae",
+                    species = "Micrurus janisrozei",
+                    biomes = "Caatinga, Cerrado, Mata Atlântica",
+                    cerrado_sp = "yes",
+                    cerrado_endemic = "no")
+
+new_species <- rbind(new_species, toAdd)
+
+toAdd <- data.frame(order = "Squamata",
+                    family = "Elapidae",
+                    species = "Micrurus bonita",
+                    biomes = "Caatinga, Cerrado, Mata Atlântica",
+                    cerrado_sp = "yes",
+                    cerrado_endemic = "no")
+
+new_species <- rbind(new_species, toAdd)
+
+toAdd <- data.frame(order = "Squamata",
+                    family = "Colubridae",
+                    species = "Philodryas pseudomamba",
+                    biomes = "Caatinga, Cerrado, Mata Atlântica, Pampa, Pantanal",
+                    cerrado_sp = "yes",
+                    cerrado_endemic = "no")
+
+new_species <- rbind(new_species, toAdd)
+
+toAdd <- data.frame(order = "Squamata",
+                    family = "Colubridae",
+                    species = "Tantilla selmae",
+                    biomes = "Cerrado, Mata Atlântica, Pampa",
+                    cerrado_sp = "yes",
+                    cerrado_endemic = "no")
+
+new_species <- rbind(new_species, toAdd)
+new_species
+
+list_cerrado <- rbind(list_cerrado, new_species)
+tail(list_cerrado)
+list_cerrado <- list_cerrado[order(list_cerrado$species),] #439
+
+list_br <- rbind(list_br, new_species)
+
+
+# Inspecting and selecting missing species to add -------------------------
+head(list_br)
+head(br_species)
+
+br_species <- br_species[,-c(2,4,6,7)]
+head(br_species)
+
+br_species$biomes <- NA
+br_species$cerrado_sp <- "no"
+br_species$cerrado_endemic <- "no"
+
+missing_to_add <- br_species[br_species$species %in% missing$species,]
+
+list_br <- rbind(list_br, missing_to_add)
+list_br <- list_br[!duplicated(list_br$species),]
+
+
+# reviewing Cerrado turtles and tortoises (TTWG 2025) ---------------------
+list_br[list_br$order =="Testudines" & list_br$cerrado_sp =="yes",]
+
+drop_cerrado_turtles <- c("Caretta caretta", "Chelonia mnydas", "Dermochelys coriacea", "Eretmochelys imbricata", "Lepidochelys olivacea", "Platemys platycephala")
+
+list_br$cerrado_sp[list_br$species %in% drop_cerrado_turtles] <- "no"
+
+table(list_br$cerrado_sp) # 434/882 = 49.2%
+
+# saving final Brazilian reptile list -------------------------------------
+write.table(list_br, here::here("data", "processed", "lists", "br_reptiles.txt"),
+            fileEncoding = "UTF8",
+            sep= "\t",
+            row.names = FALSE)
+
+###########################################################################
+# selecting only Cerrado species from the Brazilian list ------------------
+###########################################################################
+
+list_cerrado <- list_br[list_br$cerrado_sp =="yes",]
+
+table(list_cerrado$cerrado_endemic) #127/434 = 29.2% reptiles
+
+table(list_cerrado$cerrado_endemic, list_cerrado$order)
+
+127/(127+283) #127/410 = 30.9% Squamata
+
+list_cerrado[list_cerrado$order=="Crocodylia",] # ok
+list_cerrado[list_cerrado$order=="Testudines",] # review
--- R/R_data.R
+++ R/R_data.R
@@ -1,18 +1,10 @@
-#list_raw <- read.csv(here::here("data", "raw", "lists", "salve-publico-exportacao-fichas-planilha28-06-2026-20-49-24.csv"), encoding = "UTF8") 
-#list_simple <- read.table(here::here("data", "processed", "lists", "salve-simplified.txt"), h = T, sep = "\t")
-#list_br <- read.table(here::here("data", "processed", "lists", "salve-taxonomy-reviewed.txt"), h = T, sep = "\t")
+#list_raw <- read.csv(here::here("data", "raw", "lists", "salve-publico-exportacao-fichas-planilha28-06-2026-20-49-24.csv"), encoding = "UTF8") #requires namenclature harmonization
+#list_simple <- read.table(here::here("data", "processed", "lists", "salve-simplified.txt"), h = T, sep = "\t") #requires namenclature harmonization
+#list <- read.table(here::here("data", "processed", "lists", "salve-taxonomy-reviewed.txt"), h = T, sep = "\t") #835 29-06
 
-#list <- read.table(here::here("data", "processed", "lists", "salve-squamata-reviewed.txt"), h = T, sep = "\t") #790 29-06
-list <- read.table(here::here("data", "processed", "lists", "list_traits.txt"), h = T, sep = "\t") #790 29-06
-end <- read.table(here::here("data", "processed", "lists", "cerrado_endemics_list.txt"), h = T, sep = "\t") #125 29-06
+list_br <- read.table(here::here("data", "processed", "lists", "br_reptiles.txt"), h = T, sep = "\t")
 
-#repttraits_raw <- read.csv(here::here("data", "raw","traits", "repttraits-raw.csv"))
-#squambase_raw <- read.csv(here::here("data", "raw","traits", "squambase-raw.csv"))
-
-repttraits_br <- read.table(here::here("data", "processed","traits", "repttraits-br.txt"), header = TRUE) #790 29-06
-squambase_br <- read.table(here::here("data", "processed","traits", "squambase-br.txt"), header = TRUE) #790 29-06
-
-#traits <- read.table(here::here("data", "processed", "traits", "traits_merge.csv"), header = TRUE, sep = ",")
+#end <- read.table(here::here("data", "processed", "lists", "cerrado_endemics_list.txt"), h = T, sep = "\t") #125 29-06 from Vieira-Alencar et al 2023 with current nomenclature
 
 #tree_raw <- ape::read.tree(here::here("data", "raw","phylogeny", "main_title.tre")) #phylogenetic tree (title et al 2024)
 tree <- ape::read.tree(here::here("data", "processed","phylogeny", "title_br.tre")) #pruned to SALVE species
@@ -25,11 +17,3 @@ db_reviewed <- read.table(here::here("data", "processed", "distribution", "db_re
 brazil <- sf::st_read(here::here("data", "raw", "shapes", "brazil.shp"))
 biomas <- sf::st_read(here::here("data", "raw", "shapes", "biomas.shp"))
 #grid <- sf::st_read(here::here("data", "processed", "shapes", "grid.shp"))
-
-
-# outputs -----------------------------------------------------------------
-pa <- read.csv(here::here("outputs", "pa_matrix.csv"), row.names = 1, check.names = FALSE)
-coords <- read.csv(here::here("outputs", "coords.csv"), row.names = 1, check.names = FALSE)
-grid_pa <- sf::st_read(here::here("data", "processed", "shapes", "grid_pa.shp"))
-out_single_site <- readRDS(here::here("outputs", "sbears_single.rds"))
-
