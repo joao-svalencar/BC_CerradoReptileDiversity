ffafc0cea8aae8c41b5ad9da2e8a3359e5a98301
joao-svalencar <joaopaulo.valencar@gmail.com>
2026-07-28 19:43:39 +1000

    joined and improved reproductilibility of S_figure scripts

--- R/S_Figs.R
+++ R/S_Figs.R
@@ -1,16 +1,70 @@
 
-# figure endemics ---------------------------------------------------------
-list_cerrado <- list_br[list_br$cerrado_sp=="yes",]
-
-table(list_cerrado$order)
-table(list_cerrado$cerrado_endemic, list_cerrado$order)
-
 library(ggplot2)
 library(cowplot)
 
 ###########################################################################
-# Fig. 1 - Endemism levels ------------------------------------------------
-sppRich
+# Figure 1 - NMDS Cerrado centrality --------------------------------------
+###########################################################################
+
+nmds_coordinates <- as.data.frame(nmds_res$points) # object nmds_res from D_analyses line 62
+nmds_coordinates$Biome <- rownames(nmds_coordinates)
+
+biome_colors <- c(
+  "Cerrado"         = "#E6AB02", 
+  "Atlantic Forest" = "#31A354",
+  "Caatinga"       = "#D95F02",
+  "Amazon"       = "#0B4C28",
+  "Pantanal"       = "#0072B2",
+  "Pampa"          = "#CC79A7"
+)
+
+nmds_fig <- ggplot(nmds_coordinates, aes(x = MDS1, y = MDS2, color = Biome)) +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "gray50", linewidth = 0.6) +
+  geom_vline(xintercept = 0, linetype = "dotted", color = "gray50", linewidth = 0.6) +
+  geom_point(size = 4, alpha = 0.8) +
+  geom_text(aes(label = Biome), 
+            vjust = -1, 
+            fontface = "bold", 
+            size = 4.5, 
+            show.legend = FALSE) +
+  scale_color_manual(values = biome_colors) +
+  expand_limits(
+    y = c(min(nmds_coordinates$MDS2) - 0.1, max(nmds_coordinates$MDS2) + 0.1),
+    x = c(min(nmds_coordinates$MDS1) - 0.1, max(nmds_coordinates$MDS1) + 0.1)
+  ) +
+  theme_minimal(base_size = 14) +
+  labs(
+    x = "Axis 1",
+    y = "Axis 2"
+  ) +
+  theme(
+    axis.line = element_line(color = "gray30", linewidth = 0.6),
+    legend.position = "none",
+    panel.grid.minor = element_blank()
+  )
+
+
+ggsave("nmds_fig.png",
+       device = "png",
+       plot = nmds_fig,
+       path = here::here("outputs", "figures"),
+       width = 180,
+       height = 100,
+       units = "mm",
+       dpi = 1000,
+)
+
+###########################################################################
+# Figure 6 - Endemism levels through time ---------------------------------
+###########################################################################
+
+list_cerrado <- list_br[list_br$cerrado_sp=="yes",] # object list_br from R_data line xxx
+
+table(list_cerrado$order)
+table(list_cerrado$cerrado_endemic, list_cerrado$order)
+
+
+sppRich # object sppRich from R_data line xxx
 sppRich$suborder <- factor(sppRich$suborder, levels=c("Amphisbaenia", "Sauria", "Serpentes"))
 sppRich$year <- factor(sppRich$year)
 
